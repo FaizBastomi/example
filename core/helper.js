d238672a744a0157468e8c9781edb73204c25160
FaizBastomi <faizbastomy3@gmail.com>
2021-05-07 14:42:24 +0700

    🛠️ fix weird error
    cannot read property ephemeralMessage of undefined

--- core/helper.js
+++ core/helper.js
@@ -7,26 +7,31 @@ const fs = require('fs')
 
 const wa = con.Whatsapp
 
+/**
+ * Serialize
+ * @param chat Chat
+ * @returns
+ */
 exports.serialize = function(chat) {
     m = JSON.parse(JSON.stringify(chat)).messages[0]
 
-    if(m.message['ephemeralMessage']) {
-        m.message = m.message.ephemeralMessage.message
-        m.isEphemeral = true
-    } else {
-        m.isEphemeral = false
-    }
-
-    content = m.message
-    m.isGroup = m.key.remoteJid.endsWith('@g.us')
-    m.from = m.key.remoteJid
-
+    const content = m.message
     try{
         const tipe = Object.keys(content)[0]
         m.type = tipe
     } catch {
         m.type = null
     }
+
+    if(m.type === 'ephemeralMessage') {
+        m.message = m.message.ephemeralMessage.message
+        m.isEphemeral = true
+    } else {
+        m.isEphemeral = false
+    }
+
+    m.isGroup = m.key.remoteJid.endsWith('@g.us')
+    m.from = m.key.remoteJid
 
     try{
         const quote = m.message.extendedTextMessage.contextInfo
@@ -65,23 +70,41 @@ exports.serialize = function(chat) {
     return m
 }
 
+/**
+ * Send a reply to someone
+ * @param jid of the chat
+ * @param text your text
+ * @param quoted the message you want to reply
+ */
 exports.reply = function(jid, text, quoted) {
     wa.sendMessage(jid, text, MessageType.text, { quoted: quoted })
 }
 
+/**
+ * Send text
+ * @param jid of the chat
+ * @param text your text
+ */
 exports.sendText = function(jid, text) {
     wa.sendMessage(jid, text, MessageType.text)
 }
 
+/**
+ * Send your custom message
+ * @param jid of the chat
+ * @param text your text or something else
+ * @param Messagetype your MessageType
+ * @param options MessageOptions Baileys
+ */
 exports.custom = function(jid, text, Messagetype, options={}) {
     wa.sendMessage(jid, text, Messagetype, options)
 }
 
 /**
- * 
- * @param {jid} jid of the chat
- * @param {data} data Buffer or path to file
- * @param {options} options baileys Message Options
+ * Sending image
+ * @param jid of the chat
+ * @param data Buffer or path to file
+ * @param options baileys Message Options
  */
 exports.image = function(jid, data, options={}) {
     if (typeof data === 'string') {
@@ -92,10 +115,10 @@ exports.image = function(jid, data, options={}) {
 }
 
 /**
- * 
- * @param {jid} jid of the chat
- * @param {data} data Buffer or path to file
- * @param {options} options baileys Message Options
+ * Sending sticker
+ * @param jid of the chat
+ * @param data Buffer or path to file
+ * @param options baileys Message Options
  */
 exports.sticker = function(jid, data, options={}) {
     if (typeof data === 'string') {
